eecf05f8e1fe1fc8ad026a5b0b66a6f513f94a40
venkatasujatha <valluru.1sujatha@gmail.com>
2022-12-27 14:56:20 +0530

    one-to-many

--- controller/studentController.js
+++ controller/studentController.js
@@ -25,15 +25,20 @@ const insert = async (req, res) => {
   try {
     student_name = req.body.student_name;
     age = req.body.age;
-    const sql = await database.query(
-      `INSERT INTO studentDetails(student_name,age) VALUES ('${student_name}','${age}');
-        INSERT INTO deptDetails(dept_name,address,id) VALUES ('${req.body.dept_name}','${req.body.address}',${req.body.id});`
-    );
+    //one-to-one
+    // const sql = await database.query(
+    //   `INSERT INTO studentDetails(student_name,age) VALUES ('${student_name}','${age}');
+    //     INSERT INTO deptDetails(dept_name,address,id) VALUES ('${req.body.dept_name}','${req.body.address}',${req.body.id});`
+    // );
+// //one-to-many
+const sql = await database.query(
+  `INSERT INTO studentDetails(student_name,age) VALUES ('${student_name}','${age}');
+    INSERT INTO deptDetails(dept_name,address,id) VALUES ('${req.body.dept_name[0]}','${req.body.address}',${req.body.id}),('${req.body.dept_name[1]}','${req.body.address}',${req.body.id});`
+);
 
-    console.log("result", sql);
     res.status(200).json({
       message: "inserted records into db",
-      res: sql.rowCount,
+      res:sql.rowCount
     });
   } catch (error) {
     console.log(error);
@@ -109,53 +114,46 @@ const findall = async (req, res) => {
 const findOneStudent = async (req, res) => {
   try {
     student_name = req.body.student_name;
-    age =req.body.age
-    if(student_name)
-    {
-        const sql = await database.query(
-            `select *
+    age = req.body.age;
+    if (student_name) {
+      const sql = await database.query(
+        `select *
                   FROM studentdetails
                   INNER JOIN deptDetails ON studentdetails.id = deptDetails.dept_id  WHERE studentDetails.student_name ='${student_name}' `
-            //   `select * from studentDetails where student_name='${student_name}'
-            //   `
-          );
-          console.log("rows in db", sql.rows);
-          if (sql.rows < "1") {
-            console.log("no rows in db");
-          } else {
-            console.log("fetch records from db", sql);
-            res.status(200).json({
-              message: "fetch records from db",
-              res: sql.rows,
-            });
-          }
-
-    }
-    else if(age)
-    {
-        const sql = await database.query(
-            `select *
+        //   `select * from studentDetails where student_name='${student_name}'
+        //   `
+      );
+      console.log("rows in db", sql.rows);
+      if (sql.rows < "1") {
+        console.log("no rows in db");
+      } else {
+        console.log("fetch records from db", sql);
+        res.status(200).json({
+          message: "fetch records from db",
+          res: sql.rows,
+        });
+      }
+    } else if (age) {
+      const sql = await database.query(
+        `select *
                   FROM studentdetails
                   INNER JOIN deptDetails ON studentdetails.id = deptDetails.dept_id  WHERE studentDetails.age ='${age}' `
-            //   `select * from studentDetails where student_name='${student_name}'
-            //   `
-          );
-          console.log("rows in db", sql.rows);
-          if (sql.rows < "1") {
-            console.log("no rows in db");
-          } else {
-            console.log("fetch records from db", sql);
-            res.status(200).json({
-              message: "fetch records from db",
-              res: sql.rows,
-            });
-          }
+        //   `select * from studentDetails where student_name='${student_name}'
+        //   `
+      );
+      console.log("rows in db", sql.rows);
+      if (sql.rows < "1") {
+        console.log("no rows in db");
+      } else {
+        console.log("fetch records from db", sql);
+        res.status(200).json({
+          message: "fetch records from db",
+          res: sql.rows,
+        });
+      }
+    } else {
+      console.log("please provide valid details");
     }
-    else
-    {
-        console.log("please provide valid details")
-    }
-   
   } catch (error) {
     console.log("unable fetch records from db");
     console.log(error.message);
@@ -170,42 +168,37 @@ const updateStudent = async (req, res) => {
   try {
     student_name = req.body.student_name;
     age = req.body.age;
-    dept_name =req.body.dept_name
-    address =req.body.address
-    if(dept_name)
-    {
-        const sql = await database.query(
-            //`update studentDetails set student_name ='${student_name}' where age='${age}'`
-            `update deptDetails  set dept_name ='${dept_name}' where  id= ${req.body.id}`
-          );
-      
-          console.log("update name field successfully", sql.rowCount);
-          res.status(200).json({
-            message: "update record successfully",
-            res: sql.rowCount,
-          });
+    dept_name = req.body.dept_name;
+    address = req.body.address;
+    if (dept_name) {
+      const sql = await database.query(
+        //`update studentDetails set student_name ='${student_name}' where age='${age}'`
+        `update deptDetails  set dept_name ='${dept_name}' where  id= ${req.body.id}`
+      );
+
+      console.log("update name field successfully", sql.rowCount);
+      res.status(200).json({
+        message: "update record successfully",
+        res: sql.rowCount,
+      });
+    } else if (address) {
+      const sql = await database.query(
+        //`update studentDetails set student_name ='${student_name}' where age='${age}'`
+        `update deptDetails  set address ='${address}' where  id= ${req.body.id}`
+      );
+
+      console.log("update address field successfully", sql.rowCount);
+      res.status(200).json({
+        message: "update record successfully",
+        res: sql.rowCount,
+      });
+    } else {
+      console.log("please provide valid details");
+      res.status(400).json({
+        message: "please provide valid details",
+      });
     }
-    else if(address)
-    {
-        const sql = await database.query(
-            //`update studentDetails set student_name ='${student_name}' where age='${age}'`
-            `update deptDetails  set address ='${address}' where  id= ${req.body.id}`
-          );
-      
-          console.log("update address field successfully", sql.rowCount);
-          res.status(200).json({
-            message: "update record successfully",
-            res: sql.rowCount,
-          });
-    }
-    else{
-        console.log("please provide valid details");
-        res.status(400).json({
-            message: "please provide valid details"
-          });
-    }
-   
-  } catch (error) { 
+  } catch (error) {
     console.log("unable update record in db");
     console.log(error.message);
     res.status(400).json({
@@ -218,34 +211,27 @@ const updateStudent = async (req, res) => {
 const deleteStudent = async (req, res) => {
   try {
     student_name = req.body.student_name;
-    id=req.body.id
-    if(id)
-    {
-      const sql = await database.query(`DELETE FROM deptdetails  d USING studentDetails s
+    id = req.body.id;
+    if (id) {
+      await database.query(`DELETE FROM deptdetails  d USING studentDetails s
     WHERE d.id = s.id and s.id = ${id};
-    DELETE FROM studentdetails  s WHERE s.id =${id};`
-     // `delete from studentDetails where student_name='${student_name}'`
-    );
-    console.log("deleted record from db", sql.rowCount);
-    res.status(200).json({
-      message: "deleted record from db",
-      res: sql.rowCount,
-    });
-    }
-    else if (student_name)
-    {
-      const sql = await database.query(`DELETE FROM deptdetails  d USING studentDetails s
+    DELETE FROM studentdetails  s WHERE s.id =${id};`);
+      // `delete from studentDetails where student_name='${student_name}'`
+      console.log("deleted record from db");
+      res.status(200).json({
+        message: "deleted record from db",
+      });
+    } else if (student_name) {
+      const sql =
+        await database.query(`DELETE FROM deptdetails  d USING studentDetails s
       WHERE d.id = s.id and s.student_name = '${student_name}';
-  DELETE FROM studentdetails  s WHERE s.student_name ='${student_name}';`
-       // `delete from studentDetails where student_name='${student_name}'`
-      );
-      console.log("deleted record from db", sql.rowCount);
-    res.status(200).json({
-      message: "deleted record from db",
-      res: sql.rowCount,
-    });
-    }
-    else{
+  DELETE FROM studentdetails  s WHERE s.student_name ='${student_name}';`);
+      // `delete from studentDetails where student_name='${student_name}'`
+      console.log("deleted record from db");
+      res.status(200).json({
+        message: "deleted record from db",
+      });
+    } else {
       console.log("please provide valid details");
       res.status(400).json({
         message: "please provide valid details",
